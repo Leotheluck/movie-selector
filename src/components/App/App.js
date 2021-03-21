f8f3b19df9fe7f775ae4e5948d4c2891c63f3e9a
Leotheluck <57661577+Leotheluck@users.noreply.github.com>
2021-03-21 11:37:39 +0100

    Adding List and Fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import List from '../List/List'
 
 function App() {
   return (
     <div className="App">
+      <List/>
     </div>
   );
 }
--- src/services/Fetch.js
+++ src/services/Fetch.js
@@ -1,3 +1,3 @@
-export function fetchMovies() {
-    
+export function fetchMovies(year) {
+
 }
