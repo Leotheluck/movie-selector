5fdeca422281b6c8a04eea5373f70a500cadbcce
Amine ZEGMOU <amine95170@gmail.com>
2021-03-26 01:30:25 +0100

    Update to dark mode

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import Footer from "../Footer/Footer";
 import List from "../List/List";
 import Login from "../Login/Login";
 import Signup from "../Signup/Signup.js";
+import Home from "../Home/Home";
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
       <Navbar />
       <Switch>
         {/*<Route path="/" exact component={List} />*/}
+        <Route path="/" exact component={Home} />
         <Route path="/connection" exact component={Login} />
-        <Route path="/inscription" component={Signup} />
+        <Route path="/inscription" exact component={Signup} />
       </Switch>
       <Footer />
     </Router>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../Sidebar/Sidebar";
+
+const Home = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+};
+
+export default Home;
