53a337e1f50e2286b92f0be5eb2c1eb2e0248ed6
LouisJanssen <57401065+LouisJanssen@users.noreply.github.com>
2021-03-26 04:47:20 +0100

    Revert "revert commit for header + footer"
    This reverts commit f1ac4ae2510f3590f756ea013b0a818ad5695bd1.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 // import Navbar from "../Header/Navbar";
 // import Footer from "../Footer/Footer";
 import List from "../List/List";
-<<<<<<< HEAD
 // import Login from "../Login/Login";
 // import Signup from "../Signup/Signup.js";
 
@@ -19,22 +18,6 @@ function App() {
     //   <Footer />
     // </Router>
     <List/>
-=======
-import Login from "../Login/Login";
-import Signup from "../Signup/Signup.js";
-
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        {/*<Route path="/" exact component={List} />*/}
-        <Route path="/connection" exact component={Login} />
-        <Route path="/inscription" component={Signup} />
-      </Switch>
-      <Footer />
-    </Router>
->>>>>>> parent of 5fdeca4 (Update to dark mode)
   );
 }
 
